90c048fa90b12dade9445fcdb028b41800d6dcb4
Jerrica VanAlstyne <jsnow07@yahoo.com>
2020-03-28 21:01:37 -0400

    created Card component

--- gameon/src/App.js
+++ gameon/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Memory from "../src/pages/Memory";
+import Memory from "../src/pages/Memory"
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
